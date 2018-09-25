eb796b718e418e3a7e23022e7d0253d05d1a32aa
Atsuki <>
2018-09-26 05:30:06 +0900

    何回か回して見た

--- out_visualize.py
+++ out_visualize.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('true.csv', names=['num1', 'num2', 'num3'])
 df2 = pd.read_csv('out2.csv', names=['num1', 'num2', 'num3'])
-plt.plot(range(0,50),df['num2'],marker="o", label = 'Analytical Result')
-plt.plot(range(0,50),df2['num2'],marker="o", label = 'NN Result')
+plt.plot(range(0,50),df['num3'],marker="o", label = 'Analytical Result')
+plt.plot(range(0,50),df2['num3'],marker="o", label = 'NN Result')
 plt.title("p_graph")
 plt.legend()
 plt.savefig('train_out.png')
